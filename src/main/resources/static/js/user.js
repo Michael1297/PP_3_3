fb931ab3d28cb0b39b27f78ba4bb939993485ff1
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2024-03-04 12:34:49 +0300

    небольшие правки

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,13 +4,17 @@ import {updateUserTable} from "./modules/table.js";
 // Инициализация при загрузке документа
 $(document).ready(
     async () => {
-        const $usersList = $(document).find('.users-list');
-        const id = $usersList.attr('data-user-id');
-        const user = await getUser(id);
+        try {
+            const $usersList = $(document).find('.users-list');
+            const id = $usersList.attr('data-user-id');
+            const user = await getUser(id);
 
-        const button = `<a type="button" class="btn btn-primary pl-5 pr-5" id="${user.id}">${user.email}</a>`;
-        $usersList.find('nav').append(button);
+            const button = `<a type="button" class="btn btn-primary pl-5 pr-5" id="${user.id}">${user.email}</a>`;
+            $usersList.find('nav').append(button);
 
-        updateUserTable(user);
+            updateUserTable(user);
+        } catch (e) {
+            console.error(e);
+        }
     }
 );
